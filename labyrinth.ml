1d601dbe2a1b3e009acc5652a132eba3eff9fd05
Filip Niksic <fniksic@mpi-sws.org>
2014-04-24 16:43:23 +0200

    A working starting point.

--- labyrinth.ml
+++ labyrinth.ml
@@ -1,18 +1,51 @@
 (* Need to solve hard cases. *)
 
-type level = {
-  name : string;
-  data : (int list) list
-}
+open Printf
 
-type result = {
-  name : string;
-  sequence : string
-}
+type level =
+  { name: string;
+    data : int array array;
+    sequence : string
+  }
+
+let parse file = [
+  { name = "Easy 6";
+    data = [|
+      [|   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0|];
+      [|   0;   0;   0;   1;   1;   1;   1;   1;   1;   1;   1;   1;   1;   0|];
+      [|   0;   0;   0;   1;   0;   0;   0;   0;   0;   0;   0;   0;   1;   0|];
+      [|   0;   1; 202;   1;   0;   1;   1;   1;   1;   1;   0;   1;   1;   0|];
+      [|   0;   1; 300;   1;   0;   1;   0;   0;   0;   1;   0;   1;   0;   0|];
+      [|   0;   1; 203;   1;   0;   1;   0;   0;   0;   1;   0;   1;   1;   0|];
+      [|   0;   1;   0;   0;   1;   1;   1;   0;   1;   1;   1;   0;   1;   0|];
+      [|   0;   1;   0;   0;   1;   1;   1;   0; 203; 200; 202;   0;   1;   0|];
+      [|   0;   1;   1;   0; 302; 200; 201;   0;   1;   1;   1;   0;   1;   0|];
+      [|   0;   0;   1;   0;   1;   1;   1;   0;   1;   1;   1;   0;   1;   0|];
+      [|   0;   1;   1;   0; 302; 203; 201;   0; 201; 302; 200;   0;   1;   0|];
+      [|   0;   1;   0;   0;   1;   1;   1;   0;   1;   1;   1;   0;   3;   0|];
+      [|   0;   1;   0;   0;   0;   1;   0;   0;   1;   1;   1;   0;   0;   0|];
+      [|   0;   1;   1; 200;   1;   1;   0; 100;   1;   1;   1; 102;   0;   0|];
+      [|   0;   1;   1; 203;   1;   1;   0;   0;   1;   1;   1;   0;   0;   0|];
+      [|   0;   0;   1; 201;   1;   0;   0; 101;   1;   2;   1; 103;   0;   0|];
+      [|   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0;   0|]
+    |];
+    sequence = ""
+  }
+]
+
+let solve level =
+  { level with sequence = "TEST" }
+
+let print results =
+  List.iter (
+    fun result ->
+      printf "{ name = \"%s\"\n  sequence = \"%s\"\n}\n" result.name result.sequence
+  )
+  results
 
 let () =
-  let data_file = open_in "data.js" in
-  let levels = parse data_file in
-  let hard_levels = (* filter hard levels *) in
-  let results = (* map solve_level hard_levels *) in
+  let file = open_in "data.js" in
+  let levels = parse file in
+  let hard_levels = (* filter hard levels *) levels in
+  let results = List.map solve hard_levels in
   print results
